0a7186ab15b8222ca1e6bbd124b667a896f12c67
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-01-24 18:04:49 -0800

    Implemented counting sort w/ support for neg nums

---
+++ algorithms/classic/sorting/counting-sort.js
@@ -0,0 +1,30 @@
+const countingSort = (arr) => {
+    // #region negative numbers support
+    let largestNegativeNumber = 0;
+    arr.forEach(elem => {
+        if(elem < largestNegativeNumber) {
+            largestNegativeNumber = elem;
+        }
+    });
+    // #endregion
+    let negativeOffset = Math.abs(largestNegativeNumber);
+    const countingArr = new Array(100); // large number so it will contain all elements
+    countingArr.fill(0);    // fill arr with 0's
+    // store count of each elem
+    arr.forEach((elem, idx) => {
+        countingArr[elem + negativeOffset] += 1;
+    });
+    // change countingArr so count now stores position of element
+    for(let i = 0; i < countingArr.length - 1; i++) {
+        countingArr[i + 1] += countingArr[i];
+    }
+    // build sorted arr
+    const sortedArr = new Array(arr.length);
+    for(let i = 0; i < arr.length; i++) {
+        sortedArr[countingArr[arr[i] + negativeOffset] - 1] = arr[i];
+    }
+    return sortedArr;
+}
+
+const testingSort = require(`./test-sorting.js`);
+testingSort.testSorting(countingSort);
