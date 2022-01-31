7bccf1bbfa91219ec34a59a5fdc73315fba9c76e
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-01-30 20:21:50 -0800

    Efficient solution - generate binary nums w/ queue

--- algorithms/freeCodeCamp/queue/binary-with-queue.js
+++ algorithms/freeCodeCamp/queue/binary-with-queue.js
@@ -8,32 +8,58 @@
         // if all elems in queue are 1
             // change all 1 to 0 except queue[0] 
             // enqueue 0
-        
+    // efficient solution (after research)
+        // enqueue 1
+        // loop n times
+            // dequeue the first element
+            // push the dequeued element + '0' into the queue
+            // push the dequeued element + '1' into the queue
+            // print or store the dequeued element
+
+// efficient solution
 const solution = (n) => {
-    const queue = [1];
-    console.log(queue.join());
-    for(let i = 0; i < n - 1; i++) {
-        let tracker = queue.length - 1;
-        let allOnes = true;
-        while(tracker >= 0) {
-            if(queue[tracker] === 0) {
-                queue[tracker] = 1;
-                for(let i = tracker + 1; i < queue.length; i++) {
-                    queue[i] = 0;
-                }
-                allOnes = false;
-                break;
-            }
-            tracker--;
-        }
-        if(allOnes) {
-            for(let i = 1; i < queue.length; i++) {
-                queue[i] = 0;
-            }
-            queue.push(0);
-        }
-        console.log(queue.join(``));
+    let output = '';
+    const queue = [];
+    queue.push(`1`);
+    for(let i = 0; i < n; i++) {
+        const temp = queue.shift();
+        queue.push(temp + `0`);
+        queue.push(temp + `1`);
+        output += `${temp}, `
     }
+    // substring to remove comma
+    console.log(output.substring(0, output.length - 2));
 }
 
 solution(15);
+
+// #region solution 1
+// const solution = (n) => {
+//     const queue = [1];
+//     console.log(queue.join());
+//     for(let i = 0; i < n - 1; i++) {
+//         let tracker = queue.length - 1;
+//         let allOnes = true;
+//         while(tracker >= 0) {
+//             if(queue[tracker] === 0) {
+//                 queue[tracker] = 1;
+//                 for(let i = tracker + 1; i < queue.length; i++) {
+//                     queue[i] = 0;
+//                 }
+//                 allOnes = false;
+//                 break;
+//             }
+//             tracker--;
+//         }
+//         if(allOnes) {
+//             for(let i = 1; i < queue.length; i++) {
+//                 queue[i] = 0;
+//             }
+//             queue.push(0);
+//         }
+//         console.log(queue.join(``));
+//     }
+// }
+//
+// solution(15);
+// #endregion
